4d44ce8d327b679ad4ef89aa0be6ff4ff4441133
Arnaud Cornet <arnaud.cornet@gmail.com>
2023-07-04 15:49:57 -0400

    Cleanups.
    Move things into fuctions

--- gps.js
+++ gps.js
@@ -1,4 +1,5 @@
-const EARTH_RADIUS_NM = 3440;
+const EARTH_RADIUS_KM = 6371;
+const NAUTICAL_MILE_M = 1852;
 
 function deg_to_rad(deg) {
     return deg * Math.PI / 180;
@@ -12,37 +13,38 @@ class GPSCoordinates {
     constructor(latitude, longitude) { this.latitude = latitude; this.longitude = longitude; }
 
     movebytime(heading, groundspeed, dt_s) {
-        let groundspeed_nmps = (groundspeed / (60 * 60));
-        if (groundspeed_nmps == Infinity || groundspeed_nmps == -Infinity) { groundspeed_nmps = 0 }
-        let distance_nm = groundspeed_nmps * dt_s;
-        this.move(heading, distance_nm);
+        const groundspeed_mps = (groundspeed * NAUTICAL_MILE_M / (60 * 60));
+        if (groundspeed_mps == Infinity || groundspeed_mps == -Infinity) { groundspeed_mps = 0 }
+        const distance_m = groundspeed_mps * dt_s;
+        this.move(heading, distance_m);
     }
 
-
-    move(heading, distance_nm) {
-        if (distance_nm == 0) {
+    move(heading, distance_m) {
+        if (distance_m == 0) {
             return;
         }
-        let heading_rad = deg_to_rad(heading);
-        let dx = Math.cos(heading_rad) * distance_nm;
-        let dy = Math.sin(heading_rad) * distance_nm;
-        let dlat_rad = dx / (EARTH_RADIUS_NM);
-        let dlng_rad = dy / (EARTH_RADIUS_NM * Math.cos(deg_to_rad(this.latitude)));
+        const heading_rad = deg_to_rad(heading);
+        const dx = Math.cos(heading_rad) * distance_m;
+        const dy = Math.sin(heading_rad) * distance_m;
+        const dlat_rad = dx / (EARTH_RADIUS_KM * 1000);
+        const dlng_rad = dy / (EARTH_RADIUS_KM * 1000 * Math.cos(deg_to_rad(this.latitude)));
 
-        let latitude = (this.latitude + rad_to_deg(dlat_rad));
-        let longitude = (this.longitude + rad_to_deg(dlng_rad));
+        const latitude = (this.latitude + rad_to_deg(dlat_rad));
+        const longitude = (this.longitude + rad_to_deg(dlng_rad));
 
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
     distance_to(gpscoord) {
-        let lat1_rad = deg_to_rad(this.latitude);
-        let lat2_rad = deg_to_rad(gpscoord.latitude);
-        let lng1_rad = deg_to_rad(this.longitude);
-        let lng2_rad = deg_to_rad(gpscoord.longitude);
+        const lat1_rad = deg_to_rad(this.latitude);
+        const lat2_rad = deg_to_rad(gpscoord.latitude);
+        const lng1_rad = deg_to_rad(this.longitude);
+        const lng2_rad = deg_to_rad(gpscoord.longitude);
         return Math.acos(Math.sin(lat1_rad) * Math.sin(lat2_rad) +
             Math.cos(lat1_rad) * Math.cos(lat2_rad) *
-            Math.cos(Math.abs(lng2_rad - lng1_rad))) * EARTH_RADIUS_NM;
+            Math.cos(Math.abs(lng2_rad - lng1_rad))) * EARTH_RADIUS_KM * 1000;
     }
 }
+
+export { GPSCoordinates, deg_to_rad };
